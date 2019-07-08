0ffe9eb13047140a4d606091146f1de56ed53327
Hdong27 <yhd943@naver.com>
2019-07-08 18:52:45 +0900

    add gson maven

--- src/main/java/com/example/demo/plan/EtriDemo.java
+++ src/main/java/com/example/demo/plan/EtriDemo.java
@@ -12,8 +12,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
- 
+
 import com.google.gson.Gson;
+
  
 public class EtriDemo {
     static public class Morpheme {
